ab8503f3fc536bb96d6bbfd16045779b774e647f
stuartlogan82 <slogan@twilio.com>
2019-09-25 14:10:53 +0100

    added sections and background colour

--- hootpower-website/src/App.js
+++ hootpower-website/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <header className="header">
-        <div class="header__logo-box">
-          <img src="owl.png" alt="Logo" class="header__logo" />
+        <div className="header__logo-box">
+          <img src="owl.png" alt="Logo" className="header__logo" />
         </div>
 
 
@@ -18,40 +18,48 @@ function App() {
         </div>
 
       </header>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <p><strong>Customer: </strong>firstName lastName</p>
-            <p><strong>Account ID: </strong>accountId</p>
-            <p><strong>Address: </strong> address</p>
+      <main>
+        <section className="section-account">
+          <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">
+              Welcome, Customer Name
+        </h2>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-header card-header-primary">
-                <h2>Energy Usage</h2>
-              </div>
-              <div className="card-body">
-                <p className="description">Hello World</p>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <p><strong>Customer: </strong>firstName lastName</p>
+                <p><strong>Account ID: </strong>accountId</p>
+                <p><strong>Address: </strong> address</p>
               </div>
             </div>
-          </div>
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="card">
+                  <div className="card-header card-header-primary">
+                    <h2>Energy Usage</h2>
+                  </div>
+                  <div className="card-body">
+                    <p className="description">Hello World</p>
+                  </div>
+                </div>
+              </div>
 
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-header card-header-primary">
-                <h2>Managing your account</h2>
+              <div className="col-sm-6">
+                <div className="card">
+                  <div className="card-header card-header-primary">
+                    <h2>Managing your account</h2>
+                  </div>
+                  <div className="card-body">
+                    <p className="description">Give us a call!</p>
+                  </div>
+                </div>
               </div>
-              <div className="card-body">
-                <p className="description">Give us a call!</p>
-              </div>
+
             </div>
           </div>
-
-        </div>
-      </div>
+        </section>
+      </main>
     </>
   );
 }
