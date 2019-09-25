43cdbc8390cd91495f047660e24e60430c73f74b
stuartlogan82 <slogan@twilio.com>
2019-09-25 13:26:43 +0100

    Logo and header

--- hootpower-website/src/App.js
+++ hootpower-website/src/App.js
@@ -3,16 +3,20 @@ import './App.scss';
 
 function App() {
   return (
-    <div className='bg-videoWrapper'>
-      <div className="bg-video">
-        <video className="bg-video__content" autoPlay muted loop>
-          <source src="Agua-natural.mp4" type="video/mp4" />
-          Your browser is not supported!
-        </video>
-      </div>  
+    <header className="header">
+      <div class="header__logo-box">
+        <img src="owl.png" alt="Logo" class="header__logo" />
+      </div>
 
-    </div>
-    
+      <div className='bg-videoWrapper'>
+        <div className="bg-video">
+          <video className="bg-video__content" autoPlay muted loop>
+            <source src="windfarm.mp4" type="video/mp4" />
+            Your browser is not supported!
+        </video>
+        </div>
+      </div>
+    </header>
   );
 }
 
