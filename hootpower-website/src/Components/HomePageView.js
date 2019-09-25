4d76dc65b35c7e51268de719556a49ac814b6d28
Adam King <aking@twilio.com>
2019-09-25 16:12:14 +0100

    Home page.

--- hootpower-website/src/Components/HomePageView.js
+++ hootpower-website/src/Components/HomePageView.js
@@ -2,7 +2,77 @@ import React from 'react';
 
 class HomePageView extends React.Component {
     render() {
-        return (<><p>Home Page!</p></>);
+        return (
+        <>
+            <div className="container">
+                <div className="section text-center">
+                    <h2 className="title">What our customers say!</h2>
+                </div>
+                <div className="team">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="team-player">
+                                <div className="card card-plain">
+                                    <div className="col-md-6 ml-auto mr-auto">
+                                        <img src="yasir.jpeg" className="img-raised rounded-circle img-fluid" />
+                                    </div>
+                                    <h4 className="card-title">Yasir
+                                        <br />
+                                        <small className="card-description text-muted">London Businessman</small> 
+                                    </h4>
+                                    <div className="card-body">
+                                        <p className="card-description">
+                                            &quot;Hoot Power revolutionised my business!!&quot;
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="col-md-4">
+                            <div className="team-player">
+                                <div className="card card-plain">
+                                    <div className="col-md-6 ml-auto mr-auto">
+                                        <img src="dpj.jpeg" className="img-raised rounded-circle img-fluid" />
+                                    </div>
+                                    <h4 className="card-title">David
+                                        <br />
+                                        <small className="card-description text-muted">Welsh Businessman</small> 
+                                    </h4>
+                                    <div className="card-body">
+                                        <p className="card-description">
+                                            &quot;I wouldn't buy my Owlectricity from anybody else!&quot;
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="col-md-4">
+                            <div className="team-player">
+                                <div className="card card-plain">
+                                    <div className="col-md-6 ml-auto mr-auto">
+                                        <img src="jeff.png" className="img-raised rounded-circle img-fluid" />
+                                    </div>
+                                    <h4 className="card-title">Jeff
+                                        <br />
+                                        <small className="card-description text-muted">Long time customer</small> 
+                                    </h4>
+                                    <div className="card-body">
+                                        <p className="card-description">
+                                            &quot;Hoot Power has levelled up the UK energy market!!&quot;
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+        );
     }
 }
 
