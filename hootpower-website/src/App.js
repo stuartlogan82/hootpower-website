9f071671b74af7a4ff948555552c5fad42a38d71
Adam King <aking@twilio.com>
2019-09-25 13:27:48 +0100

    Local commit prior to merge

--- hootpower-website/src/App.js
+++ hootpower-website/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 
 function App() {
   return (
+    <>
     <div className='bg-videoWrapper'>
       <div className="bg-video">
         <video className="bg-video__content" autoPlay muted loop>
@@ -12,7 +13,40 @@ function App() {
       </div>  
 
     </div>
-    
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <p><strong>Customer: </strong>firstName lastName</p>
+          <p><strong>Account ID: </strong>accountId</p>
+          <p><strong>Address: </strong> address</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-header card-header-primary">
+              <h2>Energy Usage</h2>
+            </div>
+            <div className="card-body">
+              <p className="description">Hello World</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-header card-header-primary">
+              <h2>Managing your account</h2>
+            </div>
+            <div className="card-body">
+              <p className="description">Give us a call!</p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    </>
   );
 }
 
